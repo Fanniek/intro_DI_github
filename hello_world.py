27c66ab49b24928285b0930b27ce7ad04980e55f
Fannie Klein <fannieklein@Fannies-MacBook.local>
2019-03-14 18:12:45 +0200

    first commit

---
+++ hello_world.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 14 18:09:32 2019
+
+@author: fannieklein
+"""
+
---
+++ hello_you.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 14 18:06:35 2019
+
+@author: fannieklein
+"""
+
